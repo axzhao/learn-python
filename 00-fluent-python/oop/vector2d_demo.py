f634da2bd98e27ae0d0e0d8d38f95329ec6f36ba
jiayizhao <jiayi.zhao@hztl3.com>
2019-08-12 18:45:13 +0800

    chap11

--- 00-fluent-python/oop/vector2d_demo.py
+++ 00-fluent-python/oop/vector2d_demo.py
@@ -1,25 +1,143 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 
+"""
+A 2-dimensional vector class
+
+    >>> v1 = Vector2d(3, 4)
+    >>> print(v1.x, v1.y)
+    3.0 4.0
+    >>> x, y = v1
+    >>> x, y
+    (3.0, 4.0)
+    >>> v1
+    Vector2d(3.0, 4.0)
+    >>> v1_clone = eval(repr(v1))
+    >>> v1 == v1_clone
+    True
+    >>> print(v1)
+    (3.0, 4.0)
+    >>> octets = bytes(v1)
+    >>> octets
+    b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
+    >>> abs(v1)
+    5.0
+    >>> bool(v1), bool(Vector2d(0, 0))
+    (True, False)
+    
+    
+Test of ``.frombytes()`` class method:
+
+    >>> v1_clone = Vector2d.frombytes(bytes(v1))
+    >>> v1_clone
+    Vector2d(3.0, 4.0)
+    >>> v1 == v1_clone
+    True
+
+
+Tests of ``format()`` with Cartesian coordinates:
+
+    >>> format(v1)
+    '(3.0, 4.0)'
+    >>> format(v1, '.2f')
+    '(3.00, 4.00)'
+    >>> format(v1, '.3e')
+    '(3.000e+00, 4.000e+00)'
+
+
+Tests of the ``angle`` method::
+
+    >>> Vector2d(0, 0).angle()
+    0.0
+    >>> Vector2d(1, 0).angle()
+    0.0
+    >>> epsilon = 10**-8
+    >>> abs(Vector2d(0, 1).angle() - math.pi/2) < epsilon
+    True
+    >>> abs(Vector2d(1, 1).angle() - math.pi/4) < epsilon
+    True
+
+
+Tests of ``format()`` with polar coordinates:
+
+    >>> format(Vector2d(1, 1), 'p')  # doctest:+ELLIPSIS
+    '<1.414213..., 0.785398...>'
+    >>> format(Vector2d(1, 1), '.3ep')
+    '<1.414e+00, 7.854e-01>'
+    >>> format(Vector2d(1, 1), '0.5fp')
+    '<1.41421, 0.78540>'
+
+
+Tests of `x` and `y` read-only properties:
+
+    >>> v1.x, v1.y
+    (3.0, 4.0)
+    >>> v1.x = 123
+    Traceback (most recent call last):
+      ...
+    AttributeError: can't set attribute
+
+
+Tests of hashing:
+
+    >>> v1 = Vector2d(3, 4)
+    >>> v2 = Vector2d(3.1, 4.2)
+    >>> hash(v1), hash(v2)
+    (7, 384307168202284039)
+    >>> len(set([v1, v2]))
+    2
+    
+    
+Tests of typecode:
+
+    >>> from vector2d_demo import Vector2d
+    >>> v1 = Vector2d(1.1, 2.2)
+    >>> dumpd = bytes(v1)
+    >>> dumpd
+    b'd\x9a\x99\x99\x99\x99\x99\xf1?\x9a\x99\x99\x99\x99\x99\x01@'
+    >>> len(dumpd)
+    17
+    >>> v1.typecode = 'f'
+    >>> dumpf = bytes(v1)
+    >>> dumpf
+    b'f\xcd\xcc\x8c?\xcd\xcc\x0c@'
+    >>> len(dumpf)
+    9
+    >>> Vector2d.typecode
+    'd'
+
+
+# END VECTOR2D_V3_DEMO
+"""
 
 from array import array
 import math
 
 
 class Vector2d:
+    __slots__ = ('__x', '__y')
+
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x) # 尽早捕获错误
-        self.y = float(y)
+        self.__x = float(x)  # 尽早捕获错误
+        self.__y = float(y)
+
+    @property  # property装饰器把读值方法标记为特性
+    def x(self):  # 读值方法与公开属性同名
+        return self.__x  # 使得可以使用self.x，而不是self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     @classmethod
     def frombytes(cls, octets):
         typecode = chr(octets[0])
-        memv= memoryview(octets[1:]).cast(typecode)
+        memv = memoryview(octets[1:]).cast(typecode)
         return cls(*memv)
 
-    def __iter__(self): # 可迭代对象，方便拆包
+    def __iter__(self):  # 可迭代对象，方便拆包
         # return yield self.x; yield self.y
         return (i for i in (self.x, self.y))
 
@@ -42,8 +160,23 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
-    def __format__(self, fmt_spec=''):
-        components = (format(c, fmt_spec) for c in self)
-        return '({}, {})'.format(*components)
+    def angle(self):
+        return math.atan2(self.y, self.x)
 
-        
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):  # 自定义的格式代码，极坐标
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:  # 直角坐标系
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+
+class ShortVector2d(Vector2d):
+    typecode = 'f'  # 覆盖类属性
