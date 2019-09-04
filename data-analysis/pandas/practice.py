5e3c1c6266a77cb7b4de8e2711cb6a4bc4a9052a
Ax <k1206@foxmail.com>
2019-09-05 00:01:25 +0800

    practice

--- data-analysis/pandas/practice.py
+++ data-analysis/pandas/practice.py
@@ -1,73 +1,15 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 
-# %%
-import pandas as pd
-import numpy as np
-
-d = {"A": ["a", np.nan, np.nan, "b", np.nan],
-     "B": ["b1", "b2", np.nan, "c1", "c2"],
-     "C": ["c1", "c2", "c3", "C4", "C5"],
-     "D": [np.nan, np.nan, np.nan, "d1", "d2"]}
-
-df = pd.DataFrame(d)
-df[["A", "B"]] = df[["A", "B"]].ffill()
-c = np.where(pd.isnull(df["D"]), df["D"], df["C"])
-d = np.where(pd.isnull(df["D"]), df["C"], df["D"])
-df["C"] = c
-df["D"] = d
-pd.set_option("display.max_columns", None)
-pd.set_option("display.max_rows", None)
-df["ID"] = df.index+1
-print(df)
 
 # %%
-import pandas as pd
-
-d = {"A": [" a ", " ", " c "],
-     "B": ["", "", ""]}
-df = pd.DataFrame(d)
-df = df.apply(lambda x: x.str.strip())
-df["source"] = "haha"
-df["B"] = df["A"].apply(lambda x: x if x == "a" else "")
-print(df)
-
-#%%
-import pandas as pd
-from pyspark import SparkContext, SparkConf, HiveContext
-from pyspark.sql.functions import *
-from pyspark.sql.window import Window
-
-sc.stop()
-sc = SparkContext()
-sqlContext = HiveContext(sc)
-
-df1 = sqlContext.createDataFrame(
-    [(1, "a", "aa", "aaa"), (2, "b", "bb", "bbb"), (3, "c", "cc", "ccc"), (4, "d", "dd", "ddd")],
-    ("id", "c1", "c2", "c3"))
-
-df2 = sqlContext.createDataFrame(
-    [(1, "1", "11", "111", "p1"), (2, "2", "22", "", "p2"), (3, "3", "33", "", "p3")], 
-    ("id", "p1", "p2", "p3", "p4"))
-
-df3 = sqlContext.createDataFrame(
-    [("a", "aa", "aaa", "p1", 0, 0), ("b", "bb", "bbb", "p1", 0, 0), ("z", "zz", "", "p9", 0, 0), ("c", "cc", "ccc", "p2", 0, 0)], 
-    ("c1", "c2", "c3", "p4", "d1_id", "d2_id"))
-
-df = df3.\
-     join(df1, (df3.c1 == df1.c1) & (df3.c2 == df1.c2) & (df3.c3 == df1.c3), "inner").\
-     join(df2, (df3.p4 == df2.p4), "inner").\
-     select(lit(0).alias('id'), df3.c1, df3.c2, df3.c3, df3.p4, df1.id.alias("d1_id"), df2.id.alias("d2_id")).\
-     filter((df1.id > 0) & (df2.id > 0))
-df = df.withColumn("id", row_number().over(Window.partitionBy("id").orderBy("id")))
-df.show()
-sc.stop()
-
-#%%
 
 
-import pandas as pd
-import numpy as np
+# %%
+
+
+# %%
+
 
 d = {"A": ["a", "b", "c", "d"],
      "B": ["z", "z", "z", "z"]}
@@ -82,7 +24,7 @@ print(df[df.index.isin(uni).any()])
 print(df)
 # print(1 if df.loc[('a', 'z')].index.isin(uni).any() else 0)
 
-#%%
+# %%
 
 d = {"A": ["a", "b", "b", "a"],
      "B": ["z", "z", "z", "z"],
@@ -91,15 +33,15 @@ df = pd.DataFrame(d)
 df = df.drop_duplicates(["A", "B", "C"][:-1])
 print(df)
 
-#%%
+# %%
 
 d = {"A": ["a", "b", "b", "a"],
      "B": ["z", "z", "z", "z"],
      "C": [1, 2, 3, 4]}
 df = pd.DataFrame(d)
-print(list(zip(df.iloc[0,:], df.iloc[1,:], df.iloc[2,:])))
+print(list(zip(df.iloc[0, :], df.iloc[1, :], df.iloc[2, :])))
 
-#%%
+# %%
 
 # head = [['x', 1, 1, 1], ['y', 1, 1, 1], ['z', 1, 2, 3]]
 # df = pd.DataFrame(head)
@@ -114,7 +56,7 @@ print(list(zip(df.iloc[0,:], df.iloc[1,:], df.iloc[2,:])))
 # df.drop(0, axis=1, inplace=True)
 # body = pd.DataFrame(df.values.T, index=df.columns, columns=col)
 
-axisX = [['x', 1,1,1,1], ['y', 1,1,1,1], ['z', 1,2,3,4]]
+axisX = [['x', 1, 1, 1, 1], ['y', 1, 1, 1, 1], ['z', 1, 2, 3, 4]]
 axisX = pd.DataFrame(axisX)
 col = axisX.columns
 axisX.drop(0, axis=1, inplace=True)
@@ -132,8 +74,8 @@ df.columns = col[:-1]
 point = np.column_stack(np.where(df))
 # print(point)
 
-pointY = point[:,:1].ravel()
-pointX = point[:,1:].ravel()
+pointY = point[:, :1].ravel()
+pointX = point[:, 1:].ravel()
 # print(pointX)
 # print(pointY)
 
@@ -193,13 +135,13 @@ print([axisY[j] for j in pointY])
 # print(dfT.groupby(dfT.iloc[:,:3]).apply())
 # print(dfT.iloc[:,3:])
 
-#%%
+# %%
 
 
-#%%
+# %%
 
-d = [[1,2,3], [4,5,6]]
+d = [[1, 2, 3], [4, 5, 6]]
 dd = pd.DataFrame(d)
 print(dd)
 
-#%%
+# %%
